69d3671a95e170181cf43b36cb5e385bcf454251
Dmitriy Panin <panin.da27@gmail.com>
2023-05-24 12:48:42 +0300

    Create Parsing_DATA_02_SAK_firmwares.py
    Parser for http://mqbtools.nl/soundaktor/

---
+++ Soundaktor/Parsing_DATA_02_SAK_firmwares.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import re
+import time
+
+
+if sys.version_info[0] < 3:
+    print("Install Python 3 or newer!")
+    sys.exit(1)
+    
+def creation_directory_extract(filename):
+    path = f"SAK_DATA_02"
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    print(f"Created directory extract: {path}")
+
+def extract_data_02(filename, num_file):
+    try:
+        result_data_02 = search_data_02(filename)
+        creation_directory_extract(filename)
+        output_file_name = f"SAK_DATA_02/{filename.split('.')[0]}_DATA_02.bin"
+        with open(output_file_name, 'wb') as out_file:
+            out_file.write(result_data_02)
+            write_meta_info_data_02_file(f"{num_file}) {output_file_name}")
+    except Exception:
+        sys.exit("\nError parsing files.")
+    else:
+        print("Finished writing output files.")
+
+def write_meta_info_data_02_file(filename):
+    with open("SAK_DATA_02/meta_extract_data_02_firmwares_SAK.txt", "a+") as firmware_names:
+        print(filename, file=firmware_names)
+        
+def search_work_files(directory, num_file=1, flag=False):
+    # Search files in current directory
+    for current_dir, dirs, files in os.walk(directory): 
+        for file in files:
+            if file[-4:] == ".odx":
+                print(f"\nFile found {file:*^50}")
+                extract_data_02(file, num_file)
+                flag = True
+                num_file += 1
+    if not flag:
+        print("Files with extension .ODX not found.\n")
+        input("Press Enter to exit...")
+        sys.exit(0)
+        
+def search_data_02(file, data_02=None):
+    with open(f'{file}', 'r', encoding='utf-8') as file:
+        for line in file:
+            response = re.findall(r'<DATA>([A-Za-z0-9]{114688})</DATA>', f'{line}')
+            if response:
+                data_02 = bytearray.fromhex(''.join(response))
+                break
+        if data_02:
+            return data_02
+        print("Extract DATA_02 not found.")
+        input("Press Enter to exit...")
+        sys.exit(0)
+   
+def parsing_odx_files(directory):
+    search_work_files(directory)
+    print("\nDone's.")
+    input("Press Enter to exit...")
+
+if __name__ == "__main__":
+    # Current working directory
+    directory = os.getcwd()
+    meta_file = "SAK_DATA_02/meta_extract_data_02_firmwares_SAK.txt"
+    if os.path.isfile(meta_file):
+        os.remove(meta_file)
+    print(f"Search in current category: {directory}\n")
+    time.sleep(1)
+    parsing_odx_files(directory)
+
+
